04d76dc132b64c72089688a312aa1158b71e96db
kainthsourav <souravkainth@outlook.com>
2020-09-23 15:20:58 +0530

    Random Class to Genrate Random number and String

--- CSharpBasic/Program.cs
+++ CSharpBasic/Program.cs
@@ -15,7 +15,7 @@ namespace CSharpBasic
             int i = b;
             float f = i;
 
-          //  Explicit / Casting Type Conversaton
+            //  Explicit / Casting Type Conversaton
             int x = 1;
             byte by = (byte)x;
             float ff = 1.0f;
@@ -32,13 +32,13 @@ namespace CSharpBasic
                 b = Convert.ToByte(num);
                 Console.WriteLine(b);
                 Operateors op = new Operateors();
-                op.Add(30,40);
+                op.Add(30, 40);
                 op.Div(10, 2);
                 op.Min(100, 50);
                 op.Mul(5, 5);
                 op.Logcial(4, 2, 6);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
@@ -61,8 +61,31 @@ namespace CSharpBasic
             var iterStatements = new IterationStatements();
             iterStatements.Iteration();
 
+            //random class --System Class
+            var random = new Random();
+            for (var index = 0; index < 10; index++)
+            {
+                Console.WriteLine(random.Next());
+            }
+            for (int index = 0; index < 10; index++)
+            {
+                //limt the random between 1 to 10
+                Console.WriteLine(random.Next(1, 10));
+            }
 
+            //Genrate String
+            const int password = 10;
+            var buffer = new Char[password];
+            for (int index = 0; index < password; index++)
+            {
+                buffer[index] = (char)('a' + random.Next(0, 26));
+            }
+
+            var pass = new string(buffer);
+            Console.WriteLine(pass);
             Console.ReadLine();
         }
+        
     }
 }
+
