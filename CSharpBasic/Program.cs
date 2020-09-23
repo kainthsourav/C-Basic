9c502a725e194b7dd61074bf82fdaff36bfe482b
kainthsourav <souravkainth@outlook.com>
2020-09-23 11:56:02 +0530

    Basic --Tyep Conversion

--- CSharpBasic/Program.cs
+++ CSharpBasic/Program.cs
@@ -10,6 +10,22 @@ namespace CSharpBasic
     {
         static void Main(string[] args)
         {
+            //Implicit Type Conversation
+            byte b = 1;
+            int i = b;
+            float f = i;
+
+            //Explicit/Casting Type Conversaton
+            int x = 300;
+            byte by = (byte)x;
+            float ff = 1.0f;
+            int y = (int)ff;
+
+            //Non Compatible Type
+            string s = "1";
+            i = Convert.ToInt32(s);
+            int j = int.Parse(s);
+
         }
     }
 }
